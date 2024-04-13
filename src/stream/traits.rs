70b2cbb7f2703bdbc11e5237878d2a0094a0a4b1
Sabi <sabitheotome@gmail.com>
2024-04-12 23:34:55 -0300

    Stream::nth is now a required method

--- src/stream/traits.rs
+++ src/stream/traits.rs
@@ -2,31 +2,26 @@ use crate::parser::traits::{Parse, Parser};
 
 pub trait Stream {
     type Item;
-    type Offset;
-    type Span;
     type Peek<'a>
     where
         Self: 'a;
+    type Offset;
+    type Span;
 
     fn offset(&self) -> Self::Offset;
     fn offset_mut(&mut self) -> &mut Self::Offset;
 
     fn skip(&mut self);
     fn advance(&mut self, offset: Self::Offset);
-
     fn retract(&mut self);
     fn go_back(&mut self, offset: Self::Offset);
 
+    fn nth(&mut self, offset: Self::Offset) -> Option<Self::Item>;
     fn next(&mut self) -> Option<Self::Item>;
-    fn peek(&self) -> Option<Self::Peek<'_>>;
     fn peek_nth(&self, offset: Self::Offset) -> Option<Self::Peek<'_>>;
+    fn peek(&self) -> Option<Self::Peek<'_>>;
 
     fn span(&self, start: Self::Offset, end: Self::Offset) -> Self::Span;
-
-    fn nth(&mut self, offset: Self::Offset) -> Option<Self::Item> {
-        self.advance(offset);
-        self.next()
-    }
 
     fn parse<T>(&mut self) -> T::Output
     where
