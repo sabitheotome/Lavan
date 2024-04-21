1a1950751c998b80de25d7068daa23766a917225
Sabi <sabitheotome@gmail.com>
2024-04-21 20:10:09 -0300

    Updated crate prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,6 @@
 pub use crate::parser::sources::{any, any_eq, any_if, any_ne, eoi, take};
 pub use crate::parser::traits::{Parse, Parser};
+pub use crate::response::adapters::{sure::Sure, unsure::Unsure};
 pub use crate::stream::adapters::SliceInput;
 pub use crate::stream::adapters::StrInput;
 pub(crate) use std::ops::ControlFlow::*;
