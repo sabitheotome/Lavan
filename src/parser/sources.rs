0ee7be9c9af43bbda14c7e438cb50aaa2ca5c4d0
Sabi <sabitheotome@tutanota.de>
2024-11-20 17:07:42 -0300

    Parser sources

--- src/parser/sources.rs
+++ src/parser/sources.rs
@@ -1,177 +1,359 @@
-use super::traits::Parser;
-use crate::{
-    response::prelude::Sure,
-    stream::traits::{Stream, StreamSlice},
-};
+use super::{super::input::prelude::*, super::response::prelude::*, prelude::*};
 use std::marker::PhantomData;
 
-/// A parser for expecting the next token to be an **End of File**
-///
-/// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
-/// See its documentation for more.
-#[must_use = "Parsers are lazy and do nothing unless consumed"]
-pub struct EOI<Str>(PhantomData<Str>);
+pub use functions::*;
 
-// TODO: Documentation
-pub fn eoi<Str>() -> EOI<Str>
-where
-    Str: Stream,
-{
-    EOI(PhantomData)
-}
+pub mod functions {
+    use super::{adapters::*, *};
 
-impl<Str> Parser for EOI<Str>
-where
-    Str: Stream,
-{
-    type Input = Str;
-    type Output = bool;
+    pub fn src<Par, I>(par: Par) -> Src<Par, I> {
+        Src(par, PhantomData)
+    }
 
-    fn parse_stream(&self, input: &mut Self::Input) -> Self::Output {
-        input.has_next()
+    // TODO: Documentation
+    pub fn nop<I: Stream>() -> Src<NOP, I> {
+        src(NOP)
+    }
+
+    // TODO: Documentation
+    pub fn eoi<I: Stream>() -> Src<EOI, I> {
+        src(EOI)
+    }
+
+    // TODO: Documentation
+    pub fn any<I: Stream>() -> Src<Any, I> {
+        src(Any)
+    }
+
+    // TODO: Documentation
+    pub fn any_if<Fun, I: Stream>(f: Fun) -> Src<AnyIf<Fun>, I>
+    where
+        Fun: Fn(&I::Item) -> bool,
+    {
+        src(AnyIf(f))
+    }
+
+    // TODO: Documentation
+    pub fn any_then<Fun, I: Stream>(f: Fun) -> Src<AnyThen<Fun>, I>
+    where
+        AnyThen<Fun>: IterativeParser<I>,
+    {
+        src(AnyThen(f))
+    }
+
+    // TODO: Documentation
+    pub fn any_eq<Rhs, I: Stream>(v: Rhs) -> Src<AnyEq<Rhs>, I>
+    where
+        I::Item: PartialEq<Rhs>,
+    {
+        src(AnyEq(v))
+    }
+
+    // TODO: Documentation
+    pub fn any_ne<Rhs, I: Stream>(v: Rhs) -> Src<AnyNe<Rhs>, I>
+    where
+        I::Item: PartialEq<Rhs>,
+    {
+        src(AnyEq(v))
+    }
+
+    // TODO: Documentation
+    pub fn take<'a, I, Ref>(size: usize) -> Src<Take<'a>, I>
+    where
+        I: StreamSlice<Slice = &'a Ref>,
+        Ref: 'a + std::ops::Index<std::ops::RangeTo<usize>> + ?Sized,
+    {
+        src(Take(size, PhantomData))
+    }
+
+    // TODO: Documentation
+    pub fn func<'a, Fun, I: Stream, Out>(f: Fun) -> Src<Func<Fun, Out>, I>
+    where
+        Out: Response,
+        Fun: FnOnce(&mut I) -> Out,
+    {
+        src(Func(f, PhantomData))
+    }
+
+    // TODO: Documentation
+    pub fn mk<T, I: Stream>() -> Src<Mk<T>, I>
+    where
+        T: Parse<I>,
+    {
+        src(Mk(PhantomData))
+    }
+
+    // TODO: Documentation
+    pub fn mkdef<T, I: Stream>() -> Src<MkDefault<T>, I>
+    where
+        T: Default,
+    {
+        src(MkDefault(PhantomData))
+    }
+
+    // TODO: Documentation
+    pub fn supply<T, I: Stream>(v: T) -> Src<Supply<T>, I> {
+        src(Supply(v))
+    }
+
+    pub fn recursive<'a, Out, Par, Fun, I>(f: Fun) -> Src<Recursive<Par>, I>
+    where
+        I: Stream,
+        Par: 'a + IterativeParserRef<I, Output = Out>,
+        Fun: Fn(Weak<dyn 'a + IterativeParserRef<I, Output = Out>>) -> Par,
+    {
+        src(Recursive {
+            parser: std::rc::Rc::new_cyclic(|weak| {
+                f(Weak {
+                    parser: weak.clone()
+                        as std::rc::Weak<dyn IterativeParserRef<I, Output = Par::Output>>,
+                })
+            }),
+        })
     }
 }
 
-/// A parser for expectingany  kind of any token besides **End of File**
-///
-/// This `struct` is created by the [`any`] method on [`sources`](crate::parser::sources).
-/// See its documentation for more.
-#[must_use = "Parsers are lazy and do nothing unless consumed"]
-pub struct Any<Str>(PhantomData<Str>);
+pub mod adapters {
+    use super::*;
+    /// A parser for ignoring the [`Response::Value`] through [`Ignorable`]
+    ///
+    /// This `struct` is created by the [`Parser::discard`] method on [`Parser`].
+    /// See its documentation for more.
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[derive(Debug)]
+    pub struct Src<Par, I>(pub(crate) Par, pub(crate) std::marker::PhantomData<I>);
 
-// TODO: Documentation
-pub fn any<Str>() -> Any<Str>
-where
-    Str: Stream,
-{
-    Any(PhantomData)
+    impl<Par, I> Src<Par, I> {
+        pub fn inner(self) -> Par {
+            self.0
+        }
+    }
+
+    #[parser_fn]
+    fn infer<par>(self: &Src<par, INPUT>) -> par::Output
+    where
+        par: IterativeParser<INPUT>,
+    {
+        parse![self.0]
+    }
+
+    impl<Par: Clone, I> Clone for Src<Par, I> {
+        fn clone(&self) -> Self {
+            Self(self.0.clone(), self.1.clone())
+        }
+    }
+
+    impl<Par: Copy, I> Copy for Src<Par, I> {}
+
+    /// A parser for expecting the next token to be an **End of File**
+    ///
+    /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+    /// See its documentation for more.
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[non_exhaustive]
+    #[derive(Debug, Clone, Copy)]
+    pub struct NOP;
+
+    /// A parser for expecting the next token to be an **End of File**
+    ///
+    /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+    /// See its documentation for more.
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[non_exhaustive]
+    #[derive(Debug, Clone, Copy)]
+    pub struct EOI;
+
+    /// A parser for expectingany  kind of any token besides **End of File**
+    ///
+    /// This `struct` is created by the [`any`] method on [`sources`](crate::parser::sources).
+    /// See its documentation for more.
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[non_exhaustive]
+    #[derive(Debug, Clone, Copy)]
+    pub struct Any;
+
+    /// A parser for expecting any token that matches a predicate
+    ///
+    /// This `struct` is created by the [`any_if`] method on [`sources`](crate::parser::sources).
+    /// See its documentation for more.
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[derive(Debug, Clone, Copy)]
+    pub struct AnyIf<Fun>(pub(crate) Fun);
+
+    /// A parser for expecting any token that matches a predicate
+    ///
+    /// This `struct` is created by the [`any_if`] method on [`sources`](crate::parser::sources).
+    /// See its documentation for more.
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[derive(Debug, Clone, Copy)]
+    pub struct AnyThen<Fun>(pub(crate) Fun);
+
+    /// A parser for expecting a token to be equal to the provided value
+    ///
+    /// This `struct` is created by the [`any_eq`] method on [`sources`](crate::parser::sources).
+    /// See its documentation for more.
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[derive(Debug, Clone, Copy)]
+    pub struct AnyEq<Item, const I: bool = false>(pub(crate) Item);
+
+    /// A parser for expecting a token to be not equal to the provided value
+    ///
+    /// This `struct` is created by the [`any_ne`] method on [`sources`](crate::parser::sources).
+    /// See its documentation for more.
+    pub type AnyNe<Item> = AnyEq<Item, true>;
+
+    /// A parser for taking a provided amount of tokens,
+    /// returning a stream slice starting from the current offset
+    ///
+    /// This `struct` is created by the [`take`] method on [`sources`](crate::parser::sources).
+    /// See its documentation for more.
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[derive(Debug, Clone, Copy)]
+    pub struct Take<'a>(pub(crate) usize, pub(crate) PhantomData<&'a ()>);
+
+    /// TODO: Documentation
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[derive(Debug, Clone, Copy)]
+    pub struct Func<Fun, Out>(pub(crate) Fun, pub(crate) PhantomData<Out>);
+
+    /// TODO: Documentation
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[non_exhaustive]
+    #[derive(Debug, Clone, Copy)]
+    pub struct Mk<T>(pub(crate) PhantomData<T>);
+
+    /// TODO: Documentation
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[derive(Debug, Clone, Copy)]
+    pub struct MkDefault<T>(pub(crate) PhantomData<T>);
+
+    /// TODO: Documentation
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[derive(Debug, Clone, Copy)]
+    pub struct Supply<T>(pub(crate) T);
+
+    pub struct Recursive<Par: ?Sized> {
+        pub(crate) parser: std::rc::Rc<Par>,
+    }
+
+    pub struct Weak<Par: ?Sized> {
+        pub(crate) parser: std::rc::Weak<Par>,
+    }
 }
 
-impl<Str> Parser for Any<Str>
-where
-    Str: Stream,
-{
-    type Input = Str;
-    type Output = Option<Str::Token>;
+mod impls {
+    use super::{adapters::*, *};
 
-    fn parse_stream(&self, input: &mut Self::Input) -> Self::Output {
+    #[parser_fn]
+    fn nop(self: &NOP) -> () {}
+
+    #[parser_fn]
+    fn eoi(self: &EOI) -> bool {
+        input.next().is_none()
+    }
+
+    #[parser_fn]
+    fn any(self: &Any) -> Option<INPUT::Item> {
         input.next()
     }
-}
 
-/// A parser for expecting any token that matches a predicate
-///
-/// This `struct` is created by the [`any_if`] method on [`sources`](crate::parser::sources).
-/// See its documentation for more.
-#[must_use = "Parsers are lazy and do nothing unless consumed"]
-pub struct AnyIf<Str, Fun>(PhantomData<Str>, Fun);
-
-// TODO: Documentation
-pub fn any_if<Str, Fun>(f: Fun) -> AnyIf<Str, Fun>
-where
-    Str: Stream,
-    Fun: Fn(&Str::Token) -> bool,
-{
-    AnyIf(PhantomData, f)
-}
-
-impl<Str, Fun> Parser for AnyIf<Str, Fun>
-where
-    Str: Stream,
-    Fun: Fn(&Str::Token) -> bool,
-{
-    type Input = Str;
-    type Output = Option<Str::Token>;
-
-    fn parse_stream(&self, input: &mut Self::Input) -> Self::Output {
-        any().filter(&self.1).parse_stream(input)
+    #[parser_fn]
+    fn any_if<Fun>(self: &AnyIf<Fun>) -> Option<INPUT::Item>
+    where
+        for<'once> Fun: FnOnce(&INPUT::Item) -> bool,
+        for<'mut> Fun: FnMut(&INPUT::Item) -> bool,
+        for<'const> Fun: Fn(&INPUT::Item) -> bool,
+    {
+        let mut peekable = input.peekable();
+        let peek = peekable.peek()?;
+        ((self.0)(peek)).then_some(peekable.next()?)
     }
-}
 
-/// A parser for expecting a token to be equal to the provided value
-///
-/// This `struct` is created by the [`any_eq`] method on [`sources`](crate::parser::sources).
-/// See its documentation for more.
-#[must_use = "Parsers are lazy and do nothing unless consumed"]
-pub struct AnyEq<Str, const I: bool = false>(PhantomData<Str>, Str::Token)
-where
-    Str: Stream;
-
-// TODO: Documentation
-pub fn any_eq<Str>(v: Str::Token) -> AnyEq<Str>
-where
-    Str: Stream,
-{
-    AnyEq(PhantomData, v)
-}
-
-impl<Str> Parser for AnyEq<Str>
-where
-    Str: Stream,
-    Str::Token: PartialEq,
-{
-    type Input = Str;
-    type Output = Option<Str::Token>;
-
-    fn parse_stream(&self, input: &mut Self::Input) -> Self::Output {
-        any().filter(|v| *v == self.1).parse_stream(input)
+    #[parser_fn]
+    fn any_then<Fun, Out>(self: &AnyThen<Fun>) -> Out
+    where
+        Out: Response,
+        for<'a> Option<INPUT::Item>: Apply<&'a Fun, Output = Out>,
+    {
+        any().then(&self.0).parse_once(input)
     }
-}
 
-/// A parser for expecting a token to be not equal to the provided value
-///
-/// This `struct` is created by the [`any_ne`] method on [`sources`](crate::parser::sources).
-/// See its documentation for more.
-type AnyNe<Str> = AnyEq<Str, true>;
-
-// TODO: Documentation
-pub fn any_ne<Str>(v: Str::Token) -> AnyNe<Str>
-where
-    Str: Stream,
-{
-    AnyEq(PhantomData, v)
-}
-
-impl<Str> Parser for AnyNe<Str>
-where
-    Str: Stream,
-    Str::Token: PartialEq,
-{
-    type Input = Str;
-    type Output = Option<Str::Token>;
-
-    fn parse_stream(&self, input: &mut Self::Input) -> Self::Output {
-        any().filter(|v| *v != self.1).parse_stream(input)
+    #[parser_fn]
+    fn any_eq<Rhs>(self: &AnyEq<Rhs>) -> Option<INPUT::Item>
+    where
+        INPUT::Item: PartialEq<Rhs>,
+    {
+        parse![any_if(|v| *v == self.0)]
     }
-}
 
-/// A parser for taking a provided amount of tokens,
-/// returning a stream slice starting from the current offset
-///
-/// This `struct` is created by the [`take`] method on [`sources`](crate::parser::sources).
-/// See its documentation for more.
-#[must_use = "Parsers are lazy and do nothing unless consumed"]
-pub struct Take<'a, Str>(Str::Offset, PhantomData<&'a Str>)
-where
-    Str: StreamSlice<'a>;
+    #[parser_fn]
+    fn any_ne<Rhs>(self: &AnyNe<Rhs>) -> Option<INPUT::Item>
+    where
+        INPUT::Item: PartialEq<Rhs>,
+    {
+        parse![any_if(|v| *v != self.0)]
+    }
 
-// TODO: Documentation
-pub fn take<'a, Str>(size: Str::Offset) -> Take<'a, Str>
-where
-    Str: StreamSlice<'a>,
-{
-    Take(size, PhantomData)
-}
+    #[parser_fn]
+    fn take<'a, Ref>(self: &Take<'a>) -> Option<&'a Ref>
+    where
+        INPUT: Stream + StreamSlice<Slice = &'a Ref>,
+        Ref: 'a + ?Sized,
+    {
+        any().del().repeat_min(self.0).slice().parse_once(input)
+    }
 
-impl<'a, Str> Parser for Take<'a, Str>
-where
-    Str: StreamSlice<'a>,
-{
-    type Input = Str;
-    type Output = Option<Str::Slice>;
+    #[parser_fn]
+    fn func<Fun, Out>(self: &Func<Fun, Out>) -> Out
+    where
+        Out: Response,
+        for<'once> Fun: FnOnce(&mut INPUT) -> Out,
+        for<'mut> Fun: FnMut(&mut INPUT) -> Out,
+        for<'const> Fun: Fn(&mut INPUT) -> Out,
+    {
+        (self.0)(input)
+    }
 
-    fn parse_stream(&self, input: &mut Self::Input) -> Self::Output {
-        let end = self.0.clone();
-        input.try_slice(input.offset(), end)
+    #[parser_fn]
+    fn mk<T>(self: &Mk<T>) -> T::Output
+    where
+        T: Parse<INPUT>,
+    {
+        T::parse(input)
+    }
+
+    #[parser_fn]
+    fn mk_default<T>(self: &MkDefault<T>) -> Sure<T>
+    where
+        T: Default,
+    {
+        Sure(T::default())
+    }
+
+    #[parser_fn]
+    fn supply<T>(self: &Supply<T>) -> Sure<T>
+    where
+        for<'mut, 'const> T: Clone,
+    {
+        when! {
+            move => Sure(self.0),
+            _ => Sure(self.0.clone()),
+        }
+    }
+
+    #[parser_fn]
+    fn recursive<Par>(self: &Recursive<Par>) -> Par::Output
+    where
+        Par: ?Sized + IterativeParserRef<INPUT>,
+    {
+        self.parser.parse_as_ref(input)
+    }
+
+    #[parser_fn]
+    fn weak<Par>(self: &Weak<Par>) -> Par::Output
+    where
+        Par: ?Sized + IterativeParserRef<INPUT>,
+    {
+        self.parser.upgrade().unwrap().parse_as_ref(input)
     }
 }
