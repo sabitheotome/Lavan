85e358f2d8530c8cd7c71b44449ca064591df655
Sabi <sabitheotome@tutanota.de>
2024-11-20 17:13:49 -0300

    Lib root module

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,39 @@
 #![allow(unused)]
-#![cfg(feature = "experimental")]
+#![cfg(feature = "unstable")]
+
+#[macro_export]
+macro_rules! or {
+    ($first:expr $(,$tail:expr)+ $(,)?) => {
+        $first$(.or($tail))+
+    }
+}
+
+#[macro_export]
+macro_rules! recursive {
+    ($ident:ident) => {
+        fn $ident()
+    }
+}
 
 pub mod parser {
-    pub(crate) mod adapters {
+    pub mod adapters {
         pub mod and;
         pub mod as_ref;
         pub mod auto_bt;
+        pub mod catch;
+        pub mod del;
         pub mod delimited;
         pub mod eq;
         pub mod filter;
-        pub mod ignore;
+        pub mod lift;
         pub mod map;
-        pub mod map_err;
+        pub mod never_fails;
+        pub mod ok;
         pub mod opt;
         pub mod or;
         pub mod owned;
         pub mod parse_str;
+        pub mod persist;
         pub mod repeat;
         pub mod slice;
         pub mod spanned;
@@ -31,20 +49,23 @@ pub mod parser {
 pub mod response {
     pub(crate) mod adapters {
         pub mod bool;
+        pub mod exception;
         pub mod option;
         pub mod result;
         pub mod sure;
         pub mod unit;
         pub mod unsure;
     }
-    pub(crate) mod adaptersv2;
     pub(crate) mod prelude;
     pub(crate) mod traits;
-    pub(crate) mod traitsv2;
     pub(crate) mod util;
 }
-pub mod stream {
-    pub mod adapters;
+pub mod input {
+    pub mod adapters {
+        pub mod cursor;
+    }
+    pub mod impls;
+    pub mod prelude;
     pub mod traits;
 }
 pub mod util {
