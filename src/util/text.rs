995362379014ab869bebfb01ce8391c8fd07149b
Sabi <sabitheotome@tutanota.de>
2024-11-20 17:11:56 -0300

    Text utility

--- src/util/text.rs
+++ src/util/text.rs
@@ -1,103 +1,319 @@
-use crate::{prelude::*, stream::traits::StrStream};
+use crate::prelude::*;
+use adapters::*;
+use lavan_proc_macros::{parser_fn, source_parser};
 
 pub fn string<'a, I: 'a + StrStream<'a>>(
     quotation_mark: char,
-) -> impl 'a + Parser<Input = I, Output = Option<&'a str>> {
+) -> impl 'a + IterativeParser<I, Output = Option<&'a str>> {
     any_ne(quotation_mark)
-        .discard()
+        .del()
         .repeat()
-        .delimited(quotation_mark, quotation_mark)
-        .discard()
+        .delimited(any_eq(quotation_mark), any_eq(quotation_mark))
+        .del()
         .slice()
 }
 
-pub fn word<'a, I: 'a + StrStream<'a, Offset = usize>>(
-    word: &'a str,
-) -> impl 'a + Parser<Input = I, Output = Option<&'a str>> {
-    take(word.len()).eq(word)
+pub mod ascii {
+    use super::*;
+    use adapters::ascii::*;
+
+    #[source_parser]
+    pub fn alphanumeric(self: &AsciiAlphanumeric) -> Option<char>
+    where
+        INPUT: Stream<Item = char>,
+    {
+        implement! {
+            eval! {
+                any_if(char::is_ascii_alphanumeric)
+            }
+        }
+
+        AsciiAlphanumeric
+    }
+
+    #[source_parser]
+    pub fn alphabetic(self: &AsciiAlphabetic) -> Option<char>
+    where
+        INPUT: Stream<Item = char>,
+    {
+        implement! {
+            eval! {
+                any_if(char::is_ascii_alphabetic)
+            }
+        }
+
+        AsciiAlphabetic
+    }
+
+    #[source_parser]
+    pub fn trim(self: &AsciiTrim) -> ()
+    where
+        INPUT: Stream<Item = char>,
+    {
+        implement! {
+            eval! {
+                any_if(char::is_ascii_whitespace).del().repeat()
+            }
+        }
+
+        AsciiTrim
+    }
+
+    #[source_parser]
+    pub fn identifier(self: &AsciiIdent) -> Option<INPUT::Slice>
+    where
+        INPUT: StreamSlice<Item = char>,
+    {
+        implement! {
+            eval! {
+                any_if(char::is_ascii_alphabetic)
+                    .or('_')
+                    .del()
+                    .and(
+                        any_if(char::is_ascii_alphanumeric)
+                            .or('_')
+                            .del()
+                            .repeat_eoi(),
+                    )
+                    .slice()
+            }
+        }
+
+        AsciiIdent
+    }
+
+    #[source_parser]
+    pub fn hex_int(self: &HexInt) -> Option<INPUT::Slice>
+    where
+        INPUT: StreamSlice<Item = char>,
+    {
+        implement! {
+            eval! {
+                any_if(char::is_ascii_hexdigit)
+                    .del()
+                    .repeat_min(1)
+                    .slice()
+            }
+        }
+
+        HexInt
+    }
+
+    #[source_parser]
+    pub fn decimal_int(self: &DecInt) -> Option<INPUT::Slice>
+    where
+        INPUT: StreamSlice<Item = char>,
+    {
+        implement! {
+            eval! {
+                any_if(char::is_ascii_digit).del().repeat_min(1).slice()
+            }
+        }
+
+        DecInt
+    }
+
+    #[source_parser]
+    pub fn decimal_float(self: &DecFloat) -> Option<INPUT::Slice>
+    where
+        INPUT: StreamSlice<Item = char>,
+    {
+        implement! {
+            eval! {
+                any_if(char::is_ascii_digit)
+                .del()
+                .repeat()
+                .and(any_eq('.').del())
+                .and(any_if(char::is_ascii_digit).del().repeat_min(1))
+                .slice()
+            }
+        }
+
+        DecFloat
+    }
 }
 
-pub fn keyword<'a, I: 'a + StrStream<'a, Offset = usize>>(
-    keyword: &'a str,
-) -> impl 'a + Parser<Input = I, Output = Option<&'a str>> {
-    word(keyword).and(
-        any_if(|c: &char| c.is_alphanumeric() || *c == '_')
-            .auto_bt()
-            .discard(),
-    )
+pub mod utf {
+    use super::*;
+
+    pub fn alphanumeric<'a, I: 'a + StrStream<'a>>() -> impl 'a + IterativeParser<I, Output = bool>
+    {
+        any_if(|c: &char| c.is_alphanumeric()).del()
+    }
+
+    pub fn alphabetic<'a, I: 'a + StrStream<'a>>() -> impl 'a + IterativeParser<I, Output = bool> {
+        any_if(|c: &char| c.is_alphanumeric()).del()
+    }
+
+    pub fn identifier<'a, I: 'a + StrStream<'a>>(
+    ) -> impl IterativeParser<I, Output = Option<&'a str>> {
+        any_if(|c: &char| c.is_alphabetic())
+            .or(any_eq('_'))
+            .del()
+            .and(
+                any_if(|c: &char| c.is_alphanumeric())
+                    .or(any_eq('_'))
+                    .del()
+                    .repeat(),
+            )
+            .slice()
+    }
+
+    pub fn trim<'a, I: StrStream<'a>>() -> impl IterativeParser<I, Output = ()> {
+        any_if(|c: &char| c.is_whitespace()).del().repeat()
+    }
+
+    pub fn int_seq<'a, I: 'a + StrStream<'a>>(
+        radix: u32,
+    ) -> impl IterativeParser<I, Output = Option<&'a str>> {
+        any_if(move |c: &char| c.is_digit(radix))
+            .del()
+            .repeat_min(1)
+            .slice()
+    }
 }
 
-pub fn ascii_keyword<'a, I: 'a + StrStream<'a, Offset = usize>>(
-    keyword: &'a str,
-) -> impl 'a + Parser<Input = I, Output = Option<&'a str>> {
-    word(keyword).and(
-        any_if(|c: &char| c.is_ascii_alphanumeric() || *c == '_')
-            .auto_bt()
-            .discard(),
-    )
+pub mod adapters {
+    pub mod ascii {
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct AsciiAlphanumeric;
+
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct AsciiAlphabetic;
+
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct AsciiIdent;
+
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct AsciiTrim;
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct HexInt;
+
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct DecInt;
+
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct DecFloat;
+    }
+    pub mod utf {
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct UtfAlphanumeric;
+
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct UtfAlphabetic;
+
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct UtfIdent;
+
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct UtfTrim;
+        /// A parser for expecting the next token to be an **End of File**
+        ///
+        /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+        /// See its documentation for more.
+        #[must_use = "Parsers are lazy and do nothing unless consumed"]
+        #[non_exhaustive]
+        #[derive(Debug, Clone, Copy)]
+        pub struct IntSeq;
+    }
+
+    /// A parser for expecting the next token to be an **End of File**
+    ///
+    /// This `struct` is created by the [`eoi`] method on [`sources`](crate::parser::sources).
+    /// See its documentation for more.
+    #[must_use = "Parsers are lazy and do nothing unless consumed"]
+    #[non_exhaustive]
+    #[derive(Debug, Clone, Copy)]
+    pub struct Surrounded;
 }
 
-pub fn identifier<'a, I: StrStream<'a>>() -> impl Parser<Input = I, Output = Option<&'a str>> {
-    any_if(|c: &char| c.is_alphabetic())
-        .or(any_eq('_'))
-        .discard()
-        .and(
-            any_if(|c: &char| c.is_alphanumeric())
-                .or(any_eq('_'))
-                .discard()
-                .repeat(),
-        )
-        .slice()
-}
+mod impls {
+    use super::*;
 
-pub fn ascii_identifier<'a, I: StrStream<'a>>() -> impl Parser<Input = I, Output = Option<&'a str>>
-{
-    any_if(char::is_ascii_alphabetic)
-        .or(any_eq('_'))
-        .discard()
-        .and(
-            any_if(char::is_ascii_alphanumeric)
-                .or(any_eq('_'))
-                .discard()
-                .repeat(),
-        )
-        .slice()
-}
+    #[parser_fn]
+    fn char<'a>(self: &char) -> bool
+    where
+        INPUT: Stream<Item = Self>,
+    {
+        any_eq(when! { move => self, _ => *self, })
+            .del()
+            .parse_once(input)
+    }
 
-pub fn trim<'a, I: StrStream<'a>>() -> impl Parser<Input = I, Output = ()> {
-    any_if(|c: &char| c.is_whitespace()).discard().repeat()
-}
-
-pub fn ascii_trim<'a, I: StrStream<'a>>() -> impl Parser<Input = I, Output = ()> {
-    any_if(char::is_ascii_whitespace).discard().repeat()
-}
-
-pub fn decimal_float<'a, I: StrStream<'a>>() -> impl Parser<Input = I, Output = Option<&'a str>> {
-    any_if(char::is_ascii_digit)
-        .discard()
-        .repeat()
-        .and(any_eq('.').discard())
-        .and(any_if(char::is_ascii_digit).discard().repeat_min(1))
-        .slice()
-}
-
-pub fn hexadecimal_integer<'a, I: StrStream<'a>>(
-) -> impl Parser<Input = I, Output = Option<&'a str>> {
-    any_if(char::is_ascii_hexdigit)
-        .discard()
-        .repeat_min(1)
-        .slice()
-}
-
-pub fn decimal_integer<'a, I: StrStream<'a>>() -> impl Parser<Input = I, Output = Option<&'a str>> {
-    any_if(char::is_ascii_digit).discard().repeat_min(1).slice()
-}
-
-pub fn radix_integer<'a, I: StrStream<'a>>(
-    radix: u32,
-) -> impl Parser<Input = I, Output = Option<&'a str>> {
-    any_if(move |c: &char| c.is_digit(radix))
-        .discard()
-        .repeat_min(1)
-        .slice()
+    #[parser_fn]
+    fn str<'a>(self: &&'a str) -> bool
+    where
+        INPUT: StreamSlice<Slice = Self>,
+    {
+        take(self.len())
+            .eq(when! { move => self, _ => *self, })
+            .del()
+            .parse_once(input)
+    }
 }
